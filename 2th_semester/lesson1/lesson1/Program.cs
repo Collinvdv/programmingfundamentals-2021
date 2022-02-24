f2a36e713db973b7b07eb9b43e2847d834c7c973
Collin Van der Vorst <collin@littlemissrobot.com>
2022-02-24 08:43:13 +0100

    code programming lesson 1 and 2

---
+++ 2th_semester/lesson1/lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿// include all of the object of the system of c#
+using System;
+
+namespace lesson1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // declare a new variable with the name firstName
+            Console.WriteLine("Can you give me the firstname:");
+            string firstName = Console.ReadLine();    // ask the user for the firstname
+            Console.WriteLine("Can you give me the lastname:");
+            string lastName = Console.ReadLine();
+            string fullName = firstName + " \"The great\" "+ lastName; // we use + to concetanate strings (concat = combination of text )
+            int age = 30;
+
+            // c# is case sensitive + end everyline of code with ; 
+            Console.WriteLine(fullName + " is " + age + " years old");
+
+        }
+    }
+}
+
+
---
+++ 2th_semester/lesson2/lesson2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace lesson2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Can you give me your firstname?");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine("Can you give me your lastname?");
+            //string lastName = Console.ReadLine();
+            //string fullName = firstName + " " + lastName + "!";
+            //fullName = $"{firstName} {lastName}!";
+            //int age = 30;
+
+            //age = age + 5;
+
+            //Console.WriteLine(fullName + "is " + age + " years old");
+
+            //double age = 30;
+            //double height = 1.94;
+
+
+
+            // 1 number = 5 , 1 number = 10
+            // O: 15
+
+            //int number1 = 5;
+            //int number2 = 10;
+            //int sum = number1 + number2;
+
+            //Console.WriteLine(number1 + number2);
+
+            //string firstname, firstName;
+
+            //firstname = "collin";
+            //firstName = firstname;
+            //string lastname = "Van der Vorst";
+            //Console.WriteLine($"{firstname} {lastname}");
+
+            //string number1 = "30";
+            //string number2 = "5";
+            //Console.WriteLine(number1 + number2);
+
+
+
+
+
+            //double number1 = 2;
+            //double number2 = 3;
+
+            //number1 = number1 + 1;
+            //number1++;
+            //++number1;
+            //number1 += 1;
+
+            //number1 = number1 - 1;
+            //number1--;
+            //number1 -= 1;
+
+            //Console.WriteLine(number1 + number2); // 5
+            //Console.WriteLine(number1 - number2); // -1
+            //Console.WriteLine(number1 * number2); // 6
+            //Console.WriteLine(number1 / number2); // 0.66666
+
+            //Console.WriteLine((5 + 5) * 2);
+
+
+
+            //int number1 = 5;
+
+            //Console.WriteLine(++number1);
+            //Console.WriteLine(number1);
+
+            //int number1 = 31;
+            //int number2 = 2;
+
+            //Console.WriteLine(number1 % number2);
+
+            //string strAge = Console.ReadLine();
+            //int age = Convert.ToInt32(strAge);
+
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //Boolean isOlderThen18 = age > 18;
+
+            //Console.WriteLine(age > 18); //greater then
+            //Console.WriteLine(age >= 18); //greater then or equal
+
+            //Console.WriteLine(age == 18); // equal to
+
+            //Console.WriteLine(age < 18); //smaller then
+            //Console.WriteLine(age <= 18); //smaller then or equal
+
+            //Console.WriteLine(age != 18); // not equal to 
+
+            //Console.WriteLine(5 + 5 + "5" + (5 + 5)); //10510
+
+
+            Console.Write("C");
+            Console.Write("O");
+            Console.Write("L");
+
+
+        }
+    }
+}
