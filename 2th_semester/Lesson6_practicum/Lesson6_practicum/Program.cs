aef42f52b1828e9df6aeadccc2922e1c359238f6
Collin Van der Vorst <collin@littlemissrobot.com>
2022-03-17 08:22:54 +0100

    Add lesson6 practicum

---
+++ 2th_semester/Lesson6_practicum/Lesson6_practicum/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+
+namespace Lesson6_practicum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// Introduction
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// Hardcoded grades = 10, 14, 18
+// O: Average is 14
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// O: How many students for this grade?
+// I: 3
+// O: Student1 result is:
+// I: 10
+// O: Student2 result is:
+// I: 14
+// O: Student3 result is:
+// I: 18
+// O: Average is 14
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// O: How many students for this grade?
+// I: 3
+// O: Student1 result is:
+// I: 10
+// O: Student2 result is:
+// I: 14
+// O: Student3 result is:
+// I: 18
+// O: Average is 14
+// O: The median is 14
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// Accountancy
+// Quarter1: 1500 -1000 -300
+// Quarter2: 1700 -800 -300
+// Quarter3: 5000 -1000 300
+// Quarter4: 1500 1000 -300
+//
+// O: Result quarter 1 is 200
+// O: Result quarter 2 is 600
+// O: Result quarter 3 is 3700
+// O: Result quarter 4 is 2200
+// O: Total result is 6700
+// O: Average per month is 558,3
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// Best day of the week
+// Monday = 5.5
+// Tuesday = 10
+// Wednesday = 7
+// Thursday = 12
+// Friday = 15
+// Saturday = 22
+// Sunday = 10
+//
+// O: Hotest day of the week is Friday with 15
+// O: Coldest day of the week is Monday with 5.5
+//
+
+
+
+
+
+
+
+
+//
+// O: How many students do you have??
+// I: 3
+// O: How many test do you have??
+// I: 2
+// O: Student1 Test1 result is:
+// I: 10
+// O: Student1 Test2 result is:
+// I: 12
+// O: Student2 Test1 result is:
+// I: 10
+// O: Student2 Test2 result is:
+// I: 18
+// O: Student3 Test1 result is:
+// I: 10
+// O: Student3 Test2 result is:
+// I: 11
+
+// O: Student 1 Average is 11
+// O: Student 2 Average is 14
+// O: Student 3 Average is 10.5
+
+// O: Test 1 Average is 10
+// O: Test 2 Average is 13.66667
+//
+
+
+
+
+
+
+
+
+
+
+// Accountancy part 2
+// Read files 2020.txt, 2021.txt and 2022.txt
+// Give me back the final result 
+
